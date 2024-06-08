892e32dc857bd2c3d4f86ce9a412cbebad39e780
潘欣 <panxin@panxins-MacBook-Air.local>
2024-06-08 22:40:06 +0800

    update spring security project

---
+++ project_files/05-security-basic/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    @GetMapping("/employees")
+    public List<String> findAll() {
+        List<String> stringList = new ArrayList<>(Arrays.asList("First element", "Second element"));
+        return stringList;
+    }
+}
